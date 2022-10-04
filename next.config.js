a129828e9c68ce923e8dc9190169daad8ebebc04
luiznaiper <luiz_naiper@hotmail.com>
2022-10-04 08:21:57 -0500

    :construction: Continue config and migrating to next

--- next.config.js
+++ next.config.js
@@ -1,21 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-  swcMinify: true,
-  env: {
-    customKey: 'customValue',
-  },
-  // basePath: '/dist'
-  compress: true,
-  async redirects() {
-    return [
-      {
-        source: '/hello',
-        destination: 'https://luisolivarez.dev',
-        permanent: true,
-      },
-    ];
-  },
 };
 
 module.exports = nextConfig;

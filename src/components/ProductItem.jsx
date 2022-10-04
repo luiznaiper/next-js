47ab78f1cbf9108237acae7d6edb9a943045aaf8
luiznaiper <luiz_naiper@hotmail.com>
2022-10-04 09:08:57 -0500

    :construction: Add api to the webpage

--- src/components/ProductItem.jsx
+++ src/components/ProductItem.jsx
@@ -1,32 +1,44 @@
 import React, { useContext } from 'react';
-import '@styles/ProductItem.scss';
-import AppContext from '../context/AppContext';
+import Image from 'next/image';
+import AppContext from '@context/AppContext';
 import addToCartImage from '@icons/bt_add_to_cart.svg';
 import addedToCartImage from '@icons/bt_added_to_cart.svg';
+import styles from '@styles/ProductItem.module.scss';
 
 const ProductItem = ({ product }) => {
-	const { state, addToCart } = useContext(AppContext);
+  const { state, addToCart } = useContext(AppContext);
 
-	const handleClick = item => {
-		console.log('in cart: ', state.cart.includes(item));
-		addToCart(item)
-	}
+  const handleClick = (item) => {
+    console.log('in cart: ', state.cart.includes(item));
+    addToCart(item);
+  };
 
-	return (
-		<div className="ProductItem">
-			<img src={product.images[0]} alt={product.title} />
-			<div className="product-info">
-				<div>
-					<p>${product.price}</p>
-					<p>{product.title}</p>
-				</div>
-				<figure className="more-clickable-area" onClick={() => handleClick(product)} >
-					{  state.cart.includes(product) ? <img className="disabled add-to-cart-btn" src={addedToCartImage} alt="added to cart" />  : <img className="add-to-cart-btn pointer" src={addToCartImage} alt="add to cart" /> }
-					
-				</figure>
-			</div>
-		</div>
-	);
-}
+  return (
+    <div className={styles.ProductItem}>
+      <Image loader={() => product.images[0]} width="100%" height="100%" layout="responsive" src={product.images[0]} alt={product.title} />
+      <div className={styles['product-info']}>
+        <div>
+          <p>${product.price}</p>
+          <p>{product.title}</p>
+        </div>
+        <figure className={styles['more-clickable-area']} onClick={() => handleClick(product)}>
+          {state.cart.includes(product) ? (
+            <Image
+              loader={() => product.images[0]}
+              width="100%"
+              height="100%"
+              layout="responsive"
+              className={(styles.disabled, styles['add-to-cart-btn'])}
+              src={addedToCartImage}
+              alt="added to cart"
+            />
+          ) : (
+            <Image loader={() => product.images[0]} width="100%" height="100%" layout="responsive" className={(styles['add-to-cart-btn'], styles.pointer)} src={addToCartImage} alt="add to cart" />
+          )}
+        </figure>
+      </div>
+    </div>
+  );
+};
 
 export default ProductItem;

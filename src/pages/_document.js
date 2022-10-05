2251164a440157a61d41709c20f4ea6b7a3a2614
luiznaiper <luiz_naiper@hotmail.com>
2022-10-05 08:39:51 -0500

    :construction: Fixing warnings

--- src/pages/_document.js
+++ src/pages/_document.js
@@ -3,9 +3,7 @@ import { Html, Head, Main, NextScript } from 'next/document';
 export default function Document() {
   return (
     <Html lang="en">
-      <Head>
-        <title>NextJS Shop</title>
-      </Head>
+      <Head>{/* <title>NextJS Shop</title> */}</Head>
       <body>
         <Main />
         <NextScript />

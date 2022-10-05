be5e2f18f9a2c7848020801ae5f1a94071180bbb
luiznaiper <luiz_naiper@hotmail.com>
2022-10-05 06:45:14 -0500

    :construction: Add checkout page

--- src/components/OrderItem.jsx
+++ src/components/OrderItem.jsx
@@ -2,6 +2,7 @@ import React, { useContext } from 'react';
 import AppContext from '@context/AppContext';
 import close from '@icons/icon_close.png';
 import styles from '@styles/OrderItem.module.scss';
+import Image from 'next/image';
 
 const OrderItem = ({ product }) => {
   const { removeFromCart } = useContext(AppContext);
@@ -13,11 +14,11 @@ const OrderItem = ({ product }) => {
   return (
     <div className={styles.OrderItem}>
       <figure>
-        <img src={product.images[0]} alt={product.title} />
+        <Image loader={() => product?.images[0]} alt={product?.title} />
       </figure>
-      <p>{product.title}</p>
-      <p>${product.price}</p>
-      <img className="pointer more-clickable-area" src={close} alt="close" onClick={() => handleRemove(product)} />
+      <p>{product?.title}</p>
+      <p>${product?.price}</p>
+      <Image className="pointer more-clickable-area" src={close} alt="close" onClick={() => handleRemove(product)} />
     </div>
   );
 };
--- src/templates/Checkout.jsx
+++ src/templates/Checkout.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import OrderItem from '@components/OrderItem';
-import '@styles/Checkout.scss';
+import styles from '@styles/Checkout.Module.scss'
 
 const Checkout = () => {
 	return (

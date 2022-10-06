689b7bc54a2e5ed733bf506bc326c9f9ef27569c
luiznaiper <luiz_naiper@hotmail.com>
2022-10-06 08:40:08 -0500

    :construction: Build project with spa config

--- next.config.js
+++ next.config.js
@@ -1,9 +1,17 @@
 /** @type {import('next').NextConfig} */
+
 const nextConfig = {
   reactStrictMode: true,
+  swcMinify: true,
   images: {
-    domains: ['placeimg.com', 'api.lorem.space', 'vuzoon.com', 'www.complementosdelcafe.com'],
+    domains: ['api.lorem.space', 'cdn.pixabay.com', 'placeimg.com', 'www.libreriahuequito.com', 'thumbs.dreamstime.com'],
   },
 };
 
-module.exports = nextConfig;
+const withPWA = require('next-pwa')({
+  dest: 'public',
+  include: ['production'],
+  register: true,
+});
+
+module.exports = withPWA(nextConfig);
